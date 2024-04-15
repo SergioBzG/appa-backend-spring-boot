9d9f215f7b50c0096eb0bf22e849864f3b689921
sergio <sebermudezg@unal.edu.co>
2024-04-15 16:54:08 -0500

    first commit

---
+++ src/test/java/com/sbz/appa/AppaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbz.appa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
