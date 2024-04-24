643bdf5e2bfb6ccc0c0d22dd7e8bc953726f6ac3
sergio <sebermudezg@unal.edu.co>
2024-04-24 11:15:12 -0500

    Refactored lines inside CarriageOrder method

--- src/main/java/com/sbz/appa/core/domain/model/CarriageOrder.java
+++ src/main/java/com/sbz/appa/core/domain/model/CarriageOrder.java
@@ -11,8 +11,6 @@ public class CarriageOrder extends Service {
     private static final int PRICE_KM = 1070;
     @Override
     public Double getPrice(double distance) {
-        if(distance == 0)
-            return 0.0;
-        return Math.round((distance * PRICE_KM) * 100) / 100d;
+        return distance == 0 ? 0.0: Math.round((distance * PRICE_KM) * 100) / 100d;
     }
 }
