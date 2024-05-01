091f2d322c84dcf8a3adb5482e1b1e54db117a17
sergio <sebermudezg@unal.edu.co>
2024-05-01 17:28:25 -0500

    refactored some parts of the code

--- src/main/java/com/sbz/appa/core/domain/model/CarriageOrder.java
+++ src/main/java/com/sbz/appa/core/domain/model/CarriageOrder.java
@@ -9,8 +9,9 @@ import lombok.*;
 @Builder
 public class CarriageOrder extends Service {
     private static final int PRICE_KM = 1070;
+
     @Override
     public Double getPrice(double distance) {
-        return distance == 0 ? 0.0: Math.round((distance * PRICE_KM) * 100) / 100d;
+        return distance == 0 ? 0.0 : Math.round((distance * PRICE_KM) * 100) / 100d;
     }
 }
